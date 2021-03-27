9be69b19238bff23b92a4697bffaf95820b15a7b
IvanTishko <ivun-tishko04@mail.ru>
2021-03-27 10:56:02 +0000

    Первый урок

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,3 @@
 print('Hello world!')
-
+print('3')
 print("проверка")
---
+++ less_1.py
@@ -0,0 +1,23 @@
+#name = input('Как тебя зовут?  ')
+#print(f'Привет {name}')
+
+#age = int(input('Сколько тебе лет?  '))
+#if age <=20:
+#  print("Да у тебя ещё седых волос нет")
+#elif age <=50:
+#  print("Да ты познал жизнь")
+#else:
+#  print('Как ты ещё жив?')
+
+a = [1, 3, 6]
+print(a[-1])
+
+b = [12, 1412354, 23652, 1323, 412]
+
+print(a+b)
+
+s = []
+s.append(10)
+print(s)
+
+print(len(b))
